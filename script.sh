fd3c66dd8dfbc2c4442267bb50e811d71c14b80f
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-26 11:14:43 +0600

    Do it yar

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR3 --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b infinity-x .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b 14/infinity .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -20,22 +20,23 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-
 # Export
-export BUILD_USERNAME=FARHAN
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME=FARHAN 
+export BUILD_HOSTNAME=crave 
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
+# fix error 
+rm -rf external/chromium-webview
+
 # Lunch
-. build/envsetup.sh
-lunch infinity_Mi439-userdebug && lunch infinity_Mi439-ap2a-userdebug 
-
-# make install
+lunch infinity_Mi439_4_19-ap3a-userdebug
+lunch infinity_Mi439_4_19-ap2a-userdebug
+lunch infinity_Mi439_4_19-userdebug
 make installclean
-
-# make bacon
 mka bacon
