6f3708741dddd24c9bcdbeb8ec3aa6f3764c8156
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-09 16:37:51 +0600

    Huh

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b a14-crd .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b a15/master .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
