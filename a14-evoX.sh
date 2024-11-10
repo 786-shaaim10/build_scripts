7e67a5cc8fa25f41f37f81b8580f89c9af6a6c21
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-10 07:29:17 +0600

    Ok

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
