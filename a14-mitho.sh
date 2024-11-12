e272e716d02fffdf34e7f15b605e86206fef5433
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-12 21:38:20 +0600

    Huh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,39 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+
+# Initialize local repository
+repo init -u https://github.com/BlissRoms/stable_releases.git -b refs/tags/v18.0-beta.5-voyager --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/786-shaaim10/local_manifests_clo -b Alpha/14 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
 /opt/crave/resync.sh
-. build/envsetup.sh
-riseup mi439 userdebug 
-make installclean
-rise b
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=shaaimgazi10
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+#lunch lineage_mi439-userdebug || lunch lineage_mi439-ap3a-userdebug || lunch lineage_mi439-ap2a-userdebug
+#echo "============="
+
+# Lunch  
+ . build/envsetup.sh
+ blissify deviceCodename
